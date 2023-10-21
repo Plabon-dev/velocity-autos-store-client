47007db41291c0ce25859efac113a6b1fea9cec1
Plabon-dev <asadplabon.dev@gmail.com>
2023-10-21 18:57:41 +0600

    NaVbar design updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,22 +12,22 @@ const Login = () => {
 
     const location = useLocation();
     const navigate = useNavigate();
-    
+
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
 
-    const handleGoogleSignIn = () =>{
+    const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
-        .then()
-        .catch(error => {
-            console.log('error', error.message);
-        } )
+            .then()
+            .catch(error => {
+                console.log('error', error.message);
+            })
     }
 
-    const {signIn} = useContext(AuthContext);
+    const { signIn } = useContext(AuthContext);
 
-    const handleLogin = e =>{
+    const handleLogin = e => {
         e.preventDefault();
         console.log(e.currentTarget);
         const form = new FormData(e.currentTarget);
@@ -35,19 +35,19 @@ const Login = () => {
         const password = form.get('password');
         console.log(email, password);
         signIn(email, password)
-        .then(result =>{
-            console.log(result.user);
+            .then(result => {
+                console.log(result.user);
 
-            navigate(location?.state ? location.state : '/');
+                navigate(location?.state ? location.state : '/');
 
-        })
-        .catch(error =>{
-            console.error(error);
-        })
+            })
+            .catch(error => {
+                console.error(error);
+            })
     }
 
     return (
-        <div className="space-y-10 py-20" style={{ backgroundImage: 'url(https://c0.wallpaperflare.com/path/37/882/321/blurred-background-bubbles-close-up-droplets-0c28203cbdac1c46a8e5ddd070beae12.jpg)'}}>
+        <div className="space-y-10 py-20" style={{ backgroundImage: 'url(https://c0.wallpaperflare.com/path/37/882/321/blurred-background-bubbles-close-up-droplets-0c28203cbdac1c46a8e5ddd070beae12.jpg)' }}>
             <div className="relative flex items-center flex-col rounded-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
                 <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
                     Log In
@@ -57,11 +57,11 @@ const Login = () => {
                 </p>
                 <form onSubmit={handleLogin} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="mb-4 flex flex-col gap-6">
-                      
+
                         <div className="relative h-11 w-full min-w-[200px]">
                             <input
                                 className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                                 type="email" name="email"
+                                type="email" name="email"
                             />
                             <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                                 Email
@@ -81,7 +81,7 @@ const Login = () => {
                     <div className="inline-flex items-center">
                         <label
                             className="relative -ml-2.5 flex cursor-pointer items-center rounded-full p-3"
-                            
+
                             data-ripple-dark="true"
                         >
                             <input
@@ -121,12 +121,15 @@ const Login = () => {
                             </p>
                         </label>
                     </div>
-                    <div><button onClick={handleGoogleSignIn} className="btn btn-xs border-black">  Google login</button></div>
-                   
+                    <div><button onClick={handleGoogleSignIn} className="btn btn-xs border-black"> <div className="flex justify-center items-center">
+                    <img className="w-5 pr-2" src="https://freesvg.org/img/1534129544.png" alt="" />
+                    <h2>Google login</h2>
+                    </div></button></div>
+
                     <input className="btn btn-block mt-5" type="submit" value="Login" />
                     <p className="mt-4 block text-center font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
                         Do not have an account?
-                       
+
                         <Link className="font-medium text-pink-500 transition-colors hover:text-blue-700" to='/register'>Register</Link>
                     </p>
                 </form>
